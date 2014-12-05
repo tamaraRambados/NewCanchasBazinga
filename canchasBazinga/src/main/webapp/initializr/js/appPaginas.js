f667a018ec8bff818331a6da8e64614a68dd87be
java <java@java-System-Product-Name>
2014-12-05 11:45:45 -0300

    pagina "dinamica"

---
+++ canchasBazinga/src/main/webapp/initializr/js/appPaginas.js
@@ -0,0 +1,13 @@
+(function(){
+	
+	var app= angular.module('myapp',['myapp-paginas']);
+	
+	app.controller('MiappControl',function($scope){
+		cancha=this;
+		$scope.mansaje="holaaaaaaaa!!!!";
+		cancha.verPaginas=function(){
+			myapp-paginas;
+		};
+		
+	});
+})();
