577c869cd57456e174c0d68da1dfd14b43671070
umantram <umantram@umantram-VPCEH13FX>
2014-12-05 09:08:19 -0300

    Ahi va lo del Responsive!

--- canchasBazinga/src/main/webapp/app.js
+++ canchasBazinga/src/main/webapp/app.js
@@ -1,25 +1,22 @@
 angular
-	.module('myapp',[])
-	.controller('controladorCanchas', function($scope,$http){
-	
-		
-
-		micanchas=this;
-		$scope.cancha={};
-		
-		micanchas.cancha={};
-		micanchas.canchaListar=function(){
-			$http.get('/canchasBazinga/api/canchas/lista').success(function(data){
-																$scope.canchas=data.canchas;
-			});
-		};
-		micanchas.canchasListar();
-		micanchas.addCanchas=fuction(){
-			var data={canchas:micanchas.cancha};
-			$http.post('/canchasBazinga/api/canchas/registrar',data).success(function(data){
-				micanchas.canchasListar();
-				micanchas.cancha={};
-			});
-		};
-
-		});
+.module('myapp',[])
+.controller('controladorCancha',['$http','$scope',function($http,$scope){
+miscancha = this;
+$scope.canchas = {};
+miscancha.cancha = {};
+miscancha.canchaListar = function(){
+$http.get('/canchaBazinga/api/canchas/lista').success(function(data){
+$scope.canchas = data.cancha;
+});
+};
+miscancha.canchaListar();
+miscancha.addCanchas = function(){
+var data = {canchas:miscancha.cancha};
+$http.post('/canchasBazinga/api/canchas/registrar',data).success(function(data){
+miscancha.canchaListar();
+miscancha.canchas = {};
+});
+};
+miscancha.remover = function(cancha){
+};
+}]);
